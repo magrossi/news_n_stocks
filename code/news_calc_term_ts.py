0fed64387cc1a7bc0d375a0705c7122bd138f53a
Marcelo Grossi <magrossi@gmail.com>
2016-07-13 22:59:32 +0100

    Calculates a time series from terms
    Time series has term, count, doc count, tf idf. So it is in fact four
    time series of the same term.

--- code/news_calc_term_ts.py
+++ code/news_calc_term_ts.py
@@ -9,10 +9,19 @@ def get_tfidf_ts(input_collection, terms, filter):
     logging.info('finding term time series with %s', filter)
     cursor = input_collection.find(filter)
     ct = 0
-    terms = {}
+    ts = {}
+    for term in terms:
+        ts[term] = []
     for dsum in cursor:
-        pass # to do
-    logging.info('finished processing in %f seconds', time.time() - start_time)
+        ct += 1
+        for term in terms:
+            if term in dsum['value']['term_counts']:
+                data_point = dsum['value']['term_counts'][term] # counts = [count, doc_count, tfidf]
+                data_point.insert(0, dsum['value']['date'])
+                ts[term].append(data_point)
+    # return dic with { term: [tseries], term: [tseries], ..}
+    return ts
+    logging.info('finished processing %d documents in %f seconds', ct, time.time() - start_time)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
@@ -31,5 +40,11 @@ if __name__ == "__main__":
     client = MongoClient(mongo_server)
     db = client[db_name]
     coll = db[collection_name]
-    get_tfidf_ts(coll, ['term1', 'term2'], filter)
+
+    terms = ['night', 'day']
+
+    logging.debug('testing get_tfidf_ts function for terms %s', ', '.join(terms))
+    ts = get_tfidf_ts(coll, terms, filter)
+    logging.debug(ts)
+
     pass
